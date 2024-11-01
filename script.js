aa7ecb985990a86c4a9e8ed8c2249e43d91af2c9
Huy Tran <huytrn13@gmail.com>
2024-11-01 10:04:06 -0500

    added css to the user input for grid size button

--- script.js
+++ script.js
@@ -6,16 +6,23 @@ function newSketch(){
     while (parseInt(userInput) > 100 || parseInt(userInput) < 1){
         userInput = prompt('Number of squares per rows? (max: 100');
     }
-
-    let numberOfRows = 960/(960/userInput) ;
+    let numberOfRows = 960/(960/userInput);
     for(let i = 1; i <= (numberOfRows); i ++){
         for(let i = 1; i <= (userInput); i++){
             let grid = document.createElement('div');
-            grid.setAttribute('style', `width:${960/userInput}px; height: ${960/userInput}px;`)
+            grid.setAttribute('style', `width:${960/userInput}px; height: ${960/userInput}px;`);
             grid.addEventListener('mouseenter', () => {
-                grid.setAttribute('class', 'coloring')
+                grid.setAttribute('class', 'coloring');
             })
             container.appendChild(grid);
         }
     }
 }
+
+let newSketchButton = document.querySelector('.newSketch');
+newSketchButton.addEventListener('mouseenter', () => {
+    newSketchButton.setAttribute('style', 'background-color: green; cursor: pointer; color: white;')
+});
+newSketchButton.addEventListener('mouseleave', () => {
+    newSketchButton.setAttribute('style', 'background-color: default;')
+});
