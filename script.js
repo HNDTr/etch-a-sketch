9228fb101e4b48574cb8966272b29fc0ddb2c8c2
Huy Tran <huytrn13@gmail.com>
2024-11-01 09:54:21 -0500

    created grids based on user input through button

---
+++ script.js
@@ -0,0 +1,21 @@
+let container = document.querySelector('.container');
+
+function newSketch(){
+    let userInput = prompt('Number of squares per rows? (min: 1, max: 100)');
+    container.innerHTML = '';
+    while (parseInt(userInput) > 100 || parseInt(userInput) < 1){
+        userInput = prompt('Number of squares per rows? (max: 100');
+    }
+
+    let numberOfRows = 960/(960/userInput) ;
+    for(let i = 1; i <= (numberOfRows); i ++){
+        for(let i = 1; i <= (userInput); i++){
+            let grid = document.createElement('div');
+            grid.setAttribute('style', `width:${960/userInput}px; height: ${960/userInput}px;`)
+            grid.addEventListener('mouseenter', () => {
+                grid.setAttribute('class', 'coloring')
+            })
+            container.appendChild(grid);
+        }
+    }
+}
